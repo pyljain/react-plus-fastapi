110cc432eabb23a4071578d87f2561d7a38c0962
Payal Jain <pyl.jain3@gmail.com>
2024-02-15 23:46:50 +0000

    Updated to include a full roundtrip

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
+# Serve index.html for the root URL
+@app.get("/")
+async def index():
+    return FileResponse('./ui/build/index.html')
+   
 # Implement this to handle question from the frontend
-# @app.get("/search")
-# def search(q: str):
-#     print(f"Query is {q}")
-#     return "test"
+@app.get("/search")
+def search(q: str):
+    print(f"Query is {q}")
+    return "test"
 
 app.mount("/", StaticFiles(directory="./ui/build"))
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [searchText, setSearchText] = useState("")
+  const [result, setResult] = useState("")
+
+  const onChange = (e) => {
+    setSearchText(e.target.value)
+  }
+
+  const search = async () => {
+    const response = await fetch(`/search?q=${searchText}`)
+    const responseText = await response.text()
+    setResult(responseText)
+    
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Showcase FastAPI with React!
-        </a>
+          <br/>
+          <input type="text" placeholder="Search" value={searchText} onChange={onChange}/>
+          <button onClick={search}> Search</button>
+          <br/>
+          <div>
+            Result: {result}
+          </div>
       </header>
     </div>
   );
